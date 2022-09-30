d17657577754c197b9a22dc5d46f287c4b983090
Евгений Мишарев <misharevea@gmail.com>
2022-09-30 15:28:08 +0300

    Добавьте файлы проекта.

---
+++ SquareColumnsReinforcement/SquareColumnsReinforcementCommand.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareColumnsReinforcement
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class SquareColumnsReinforcementCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ SquareColumnsReinforcement/SquareColumnsReinforcementWPF.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SquareColumnsReinforcement
+{
+    public partial class SquareColumnsReinforcementWPF : Window
+    {
+        public SquareColumnsReinforcementWPF()
+        {
+            InitializeComponent();
+        }
+        private void btn_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+        private void SquareColumnsReinforcementWPF_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Space)
+            {
+                DialogResult = true;
+                Close();
+            }
+
+            else if (e.Key == Key.Escape)
+            {
+                DialogResult = false;
+                Close();
+            }
+        }
+    }
+}
